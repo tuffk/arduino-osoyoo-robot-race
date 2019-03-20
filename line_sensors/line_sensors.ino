0ba6aff0b76d8f62fd71e33507a12a6b544321f2
CianDoherty <41234844+CianDoherty@users.noreply.github.com>
2019-03-20 08:33:39 +0100

    First commit
    update containing IR control

--- line_sensors/line_sensors.ino
+++ line_sensors/line_sensors.ino
@@ -2,12 +2,16 @@
 #define MSENS A0
 #define LSENS A1
 #define RSENS A2
+#include <Servo.h>
+Servo head;
+head.attach(SERVO_PIN); //servo
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(MSENS,INPUT);
   pinMode(LSENS,INPUT); 
   pinMode(RSENS,INPUT); 
+  head.attach(SERVO_PIN); //servo
   Serial.begin(9600);
 }
 
@@ -32,6 +36,6 @@ void loop() {
   // put your main code here, to run repeatedly:
   
   get_direction(read_sensor_values()); // this reads the value of the sensor and inputs it to the direction deciding function
-  
+  do_IR_Tick();
   delay(10);
 }
